6c076442329bbfd5a76873c29730515b01d64bd1
Roman Kapl <code@rkapl.cz>
2024-04-14 20:24:24 +0200

    Firs version

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace GCodeSplitter {
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length != 1) {
+				Console.Error.WriteLine("Syntax: gcode-splitter file.nc");
+				System.Environment.Exit(1);
+			}
+
+			string fileName = args[0];
+			using(var r = File.OpenText(fileName)) {
+				using(var w = new SwitchStream(fileName)) {
+					string l;
+					while((l = r.ReadLine()) != null) {
+						if (l.Trim().StartsWith("M06", StringComparison.InvariantCultureIgnoreCase)) {
+							w.NextFile();
+							w.Current.WriteLine($"({l})");
+						} else {
+							w.Current.WriteLine(l);
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ SwitchStream.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace GCodeSplitter
+{
+	public class SwitchStream: IDisposable
+	{
+		private int index;
+		private string prefix;
+		private string suffix;
+		private StreamWriter writer;
+
+		public SwitchStream(string referencePath)
+		{
+			index = 1;
+			prefix = Path.Combine(Path.GetDirectoryName(referencePath), $"{Path.GetFileNameWithoutExtension(referencePath)}_");
+			suffix = Path.GetExtension(referencePath);
+
+			ReOpen();
+		}
+
+		public TextWriter Current {
+			get {
+				return writer;
+			}
+		}
+
+		public void Dispose()
+		{
+			if (writer != null) {
+				writer.Close();
+			}
+		}
+
+		public void NextFile() {
+			index++;
+			ReOpen();
+		}
+
+		private void ReOpen() {
+			if (writer != null) {
+				writer.Close();
+				writer = null;
+			}
+			writer = File.CreateText(prefix + index.ToString() + suffix);
+		}
+	}
+}
